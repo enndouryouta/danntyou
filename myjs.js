fb554553b5ded4d1e5449b0ef0bfe2d3f170e0df
enndouryouta <131093438+enndouryouta@users.noreply.github.com>
2023-07-04 10:06:56 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,8 +2,8 @@ alert('Hello Endo!');
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2+3));
-print(1/0);
-print(0/0);
+alert(1/0);
+alert(0/0);
 alert(2+3);
 alert('2'+'3');
 alert(Number('2')+Number('3'))
